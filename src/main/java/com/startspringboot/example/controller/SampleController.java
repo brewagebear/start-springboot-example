d830d305c7dc6f501f72e2701943e3d7c223b38a
SeanSin <seansin@chungbuk.ac.kr>
2019-12-12 18:09:12 +0900

    P219까지의 구현

--- src/main/java/com/startspringboot/example/controller/SampleController.java
+++ src/main/java/com/startspringboot/example/controller/SampleController.java
@@ -1,15 +1,20 @@
 package com.startspringboot.example.controller;
 
+import com.startspringboot.example.domain.MemberVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.Timestamp;
+
 @Controller
 public class SampleController {
 
-   @GetMapping("/")
-    public String sample1(Model model){
-       model.addAttribute("greeting", "Hello Thymeleaf");
-       return "sample1";
+   @GetMapping("/sample2")
+   public void sample2(Model model){
+       MemberVO vo =
+               new MemberVO(123, "u00", "p00", "홍길동",
+                       new Timestamp(System.currentTimeMillis()));
+       model.addAttribute("vo", vo);
    }
 }
---
+++ src/main/java/com/startspringboot/example/domain/MemberVO.java
@@ -0,0 +1,17 @@
+package com.startspringboot.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+public class MemberVO {
+
+    private int mno;
+    private String mid;
+    private String mpw;
+    private String mname;
+    private Timestamp regdate;
+}
